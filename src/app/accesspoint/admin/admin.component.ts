9004c5f6b115595c358ca96f337019328452fc15
Raj Bhandari <230860116005@laxmi.edu.in>
2025-07-11 18:37:36 +0530

    resgistration add

--- src/app/accesspoint/admin/admin.component.ts
+++ src/app/accesspoint/admin/admin.component.ts
@@ -12,38 +12,70 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin.component.css',]
 })
 export class AdminComponent {
-  isLogin: boolean = true;
+  isLogin: boolean = false;
   showPassword: boolean = false;
   inputType: string = 'password';
-  username: string = '';
+  adminname: string = '';
   password: string = '';
 
   constructor(private http: HttpClient, private router: Router) { }
-
-
-
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
     this.inputType = this.showPassword ? 'text' : 'password';
   }
-  saveuser() {
 
-    const user = {
-      username: this.username,
-      password: this.password
+  signupAdmin() {
+    const admin = {
+      adminName: this.adminname,
+      password: this.password,
     };
-
-    this.http.post('http://localhost:3000/add-user', user).subscribe((response: any) => {
-      if (response.status === 'success') {
-        alert('User created successfully');
-        this.router.navigate(['dashboard'], { queryParams: { roomname: this.username } });
-        this.isLogin = true;
-      } else {
-        alert('User already exists');
-      }
+    if (this.adminname === '' || this.password === '') {
+      alert('Please fill in all fields');
+      return;
+    } else {
+      this.http.post('http://localhost:3000/add-admin', admin).subscribe((response: any) => {
+        console.log(response);
+        if (response.message === 'Admin already exists') {
+          alert(response.message);
+          this.adminname = '';
+          this.password = '';
+        } else if (response.message === 'Server error') {
+          alert('An error occurred while registering the admin. Please try again later.');
+          this.adminname = '';
+          this.password = '';
+        }
+        else if (response.message === 'Admin registered successfully') {
+          alert(response.message);
+          this.router.navigate(['dashboard'], { queryParams: { roomname: this.adminname } });
+        }
+      });
     }
-
-    )
   }
 
+  loginAdmin() {
+    // const admin = {
+    //   adminName: this.adminname,
+    //   password: this.password,
+    // };
+    // if (this.adminname === '' || this.password === '') {
+    //   alert('Please fill in all fields');
+    //   return;
+    // } else {
+    //   this.http.post('http://localhost:3000/login-admin', admin).subscribe((response: any) => {
+    //     console.error(response);
+    //     if (response.message === 'Invalid adminName or password') {
+    //       alert(response.message);
+    //       this.adminname = '';
+    //       this.password = '';
+    //     } else if (response.message === 'Server error') {
+    //       alert('An error occurred while logging in. Please try again later.');
+    //       this.adminname = '';
+    //       this.password = '';
+    //     } else if (response.message === 'Login successful') {
+    //       alert(response.message);
+    //       this.router.navigate(['dashboard'], { queryParams: { roomname: this.adminname } });
+    //     }
+    //   });
+    // }
+  }
 }
